a641d3658f2d59092e3274a0ac5f511b4adf38aa
recall-lidemin <1425889927@qq.com>
2020-03-16 15:59:34 +0800

    登录页面渲染

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
     <van-nav-bar title="登录" left-arrow @click-left="$router.go(-1)"></van-nav-bar>
+    <van-cell-group>
+      <!-- 手机号 -->
+      <van-field label="手机号" placeholder="请输入手机号"></van-field>
+      <!-- 验证码 -->
+      <van-field label="验证码" placeholder="请输入验证码">
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+    <van-button class="login" type="info" block round size="small">登录</van-button>
   </div>
 </template>
 
@@ -9,4 +19,7 @@ export default {}
 </script>
 
 <style>
+.login{
+  margin: 20px 0;
+}
 </style>
