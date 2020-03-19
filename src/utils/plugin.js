52449bf5a8bdae68b04f7b77891366c98945e440
recall-lidemin <1425889927@qq.com>
2020-03-19 15:54:21 +0800

    封装延迟处理函数

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -6,8 +6,14 @@
 export default {
   install (Vue) {
     // 重写vant的notify组件，修改其默认显示时间为800毫秒
-    Vue.prototype.$notice = function (params) {
-      return Vue.prototype.$notify({ duration: 800, ...params })
-    }
+    Vue.prototype.$notice = params => Vue.prototype.$notify({ duration: 800, ...params })
+    Vue.prototype.$sleep = sleep
   }
+}
+
+// 延迟处理函数
+function sleep (time = 500) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(), time)
+  })
 }
