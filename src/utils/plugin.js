73e37dec93e51b45dcb548254c50f95680d3fbb1
recall-lidemin <1425889927@qq.com>
2020-03-20 09:56:11 +0800

    引入dayjs封装相对时间处理过滤器

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -3,11 +3,22 @@
  * 封装各类小工具
  *
  ****/
+// 引入dayjs
+import dayjs from 'dayjs'
+// 引入语言包
+import 'dayjs/locale/zh-cn.js'
+// 引入相对时间的插件
+import relativeTime from 'dayjs/plugin/relativeTime.js'
+// dayjs扩展相对插件的方法
+dayjs.extend(relativeTime)
+
 export default {
   install (Vue) {
     // 重写vant的notify组件，修改其默认显示时间为800毫秒
     Vue.prototype.$notice = params => Vue.prototype.$notify({ duration: 800, ...params })
     Vue.prototype.$sleep = sleep
+    // Vue.filter(名称，函数)
+    Vue.filter('transTime', transTime) // 注册全局过滤器
   }
 }
 
@@ -16,4 +27,13 @@ function sleep (time = 500) {
   return new Promise(function (resolve, reject) {
     setTimeout(() => resolve(), time)
   })
+}
+
+/***
+ *
+ * 日期转换函数，转换为相对时间
+***/
+function transTime (date) {
+  // locale()本地语言包转换
+  return dayjs().locale('zh-cn').from(date) // from中是日期，或dayjs得到的时间
 }
